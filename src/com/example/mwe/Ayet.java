9b139c5bbcd6f014864c869de60d9cd091687120
bsaral <betul.saral@bil.omu.edu.tr>
2013-08-15 03:03:43 +0300

    .

--- src/com/example/mwe/Ayet.java
+++ src/com/example/mwe/Ayet.java
@@ -1,12 +1,13 @@
 package com.example.mwe;
 
+import java.util.Calendar;
+import java.util.Date;
+import java.util.TimeZone;
+
 import android.app.Activity;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
-import android.database.sqlite.SQLiteException;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -16,6 +17,7 @@ public class Ayet extends Activity {
 	
 	private static String SAMPLE_TABLE_NAME = "ayetler";
 	private SQLiteDatabase sampleDB;
+	public int i ;
 	TextView t1;
 	TextView t2;
 	Button b1;
@@ -35,33 +37,82 @@ public class Ayet extends Activity {
                 " Values (8,'aaaaa','bbbb');");
 	}
 	
-	private void seedata(){
+	
+	
+	private void seedata( int i){
 		t1 = (TextView)findViewById(R.id.ayet1);
-		Cursor cursor = sampleDB.rawQuery("SELECT ayet FROM " + SAMPLE_TABLE_NAME, null);
-
-		if (cursor != null)
-		{
-		   cursor.moveToFirst();
+		t2 = (TextView)findViewById(R.id.sayfa2);
+		Cursor cursor = sampleDB.rawQuery("SELECT * FROM " + SAMPLE_TABLE_NAME, null);
 		
-		
-		       String personName = cursor.getString(cursor.getColumnIndex("ayet"));
-		       t1.setText(personName);
-		       
-		       cursor.close();
+			if (cursor != null)
+			{
+			   cursor.moveToPosition(i);
+			
+			
+			       String ayet = cursor.getString(cursor.getColumnIndex("ayet"));
+			       String sayfa = cursor.getString(cursor.getColumnIndex("sayfa"));
+			       
+			      
+			       t1.setText(ayet);
+			       t2.setText(sayfa);
+			       
+			       cursor.close();
 			}
+	   
 	}
 	
+	
+	 public int daysBetween(Date d1, Date d2){
+		 
+		 return (int)( (d2.getTime() - d1.getTime()) / (1000 * 60 * 60 * 24));
+	}
+	 
 	
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.ayet);
 		
 		
-		t2 = (TextView)findViewById(R.id.sayfa2);
-		
 		sampleDB = openOrCreateDatabase("mwe.db", MODE_PRIVATE, null);
         createTable();
-        seedata();
+        
+        TimeZone tzTarget = TimeZone.getTimeZone("Europe/Istanbul"); 
+        Calendar cal1 = Calendar.getInstance(tzTarget);
+		Calendar cal2 = Calendar.getInstance(tzTarget);
+		cal1.setTimeInMillis(System.currentTimeMillis());
+		cal1.set(Calendar.HOUR, 0);
+		cal1.set(Calendar.MINUTE, 0);
+		cal1.set(Calendar.SECOND, 0);
+		cal2.setTimeInMillis(System.currentTimeMillis());
+		cal2.set(Calendar.YEAR,2013);
+		cal2.set(Calendar.MONTH, 9);
+		cal2.set(Calendar.DAY_OF_MONTH, 15);
+		cal2.set(Calendar.HOUR, 0);
+		cal2.set(Calendar.MINUTE, 0);
+		cal2.set(Calendar.SECOND, 0);
+		
+		
+		int sonuc = daysBetween(cal1.getTime(),cal2.getTime());
+		
+		
+		if(sonuc >= 60){
+			
+			int bol = sonuc / 60 ;
+			int carp = 60 * bol ;
+			int son = sonuc - carp ;
+			seedata(son);
+		}
+		
+		else {
+			
+			seedata(sonuc);
+		}
+			
+		
+
+
+        
+        
         
 		
 			
